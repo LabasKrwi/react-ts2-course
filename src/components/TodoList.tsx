6a514f6de925273f678ec472ef0166b76e4ad0eb
Илья Коперман <ilukhankoperman@gmail.com>
2025-01-13 13:57:01 +0300

    1.0.0

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,26 @@
+import { ITodo } from "../types/data"
+import { TodoItem } from "./TodoItem";
+
+interface ITodoListProps {
+    items: ITodo[];
+    toggleTodo: (id: number) => void;
+    removeTodo: (id:number) => void;
+}
+
+const TodoList: React.FC<ITodoListProps> = (props) => {
+    const {items, removeTodo, toggleTodo} = props;
+
+    return <div>
+        {
+            items.map(todo => (
+            <TodoItem 
+                key={todo.id} 
+                removeTodo={removeTodo} 
+                toggleTodo={toggleTodo} 
+                {...todo} />
+            ))
+        }
+    </div>
+}
+
+export {TodoList}
